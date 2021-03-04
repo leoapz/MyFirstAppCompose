8ba269f29748b0be28f7e682e47bc08fad6e7eed
Leonardo Apaza <leoapaza2@gmail.com>
2021-03-04 18:21:00 -0300

    State in Compose

--- app/src/main/java/com/example/myfirstappcompose/MainActivity.kt
+++ app/src/main/java/com/example/myfirstappcompose/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,17 +36,37 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+
+    val count = remember { mutableStateOf(0) }
+
+    Button(onClick = { count.value++ }) {
+        Text("I've been clicked ${count.value} times")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Leo", "Julia", "Lula", "Americo")) {
+    val counterState = remember { mutableStateOf(0) }
+
     Column {
         for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(
+            count = counterState.value,
+            updateCount = {
+                newCount ->
+                counterState.value = newCount
+            }
+        )
     }
 }
 
