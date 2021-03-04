cb52a6ae3022678f99481fc60c6767566a7bdf60
Leonardo Apaza <leoapaza2@gmail.com>
2021-03-04 18:52:46 -0300

    Animating your list

--- app/src/main/java/com/example/myfirstappcompose/MainActivity.kt
+++ app/src/main/java/com/example/myfirstappcompose/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.myfirstappcompose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,7 +72,16 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Composable
@@ -71,20 +89,13 @@ fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
     val counterState = remember { mutableStateOf(0) }
 
     Column(modifier = Modifier.fillMaxHeight()) {
-        Column(modifier = Modifier.weight(1f)) {
-            for (name in names) {
-                Greeting(name = name)
-                Divider(color = Color.Black)
-            }
-        }
-
-        Counter(
-            count = counterState.value,
-            updateCount = {
-                newCount ->
-                counterState.value = newCount
-            }
-        )
+        NameList(names, modifier = Modifier.weight(1f))
+            Counter(
+                count = counterState.value,
+                updateCount = { newCount ->
+                    counterState.value = newCount
+                }
+            )
     }
 }
 
